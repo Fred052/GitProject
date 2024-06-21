41bda5f27ec483a487c5c7b59a1085a3992e58c0
Fred052 <ferid.suleymanzade05@gmail.com>
2024-06-21 19:19:18 +0300

    Created seperate ModelView

---
+++ GitProject/ModelView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelView.swift
+//  GitProject
+//
+//  Created by Ferid Suleymanzade on 21.06.24.
+//
+
+import SwiftUI
+
+struct ModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelView()
+}
